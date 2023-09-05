244c5a46f32aa77aa8b0554778232d875e7011cc
Shingo Isa <isashingo12@icloud.com>
2023-09-05 14:25:36 +0900

    更新機能実装終了しました

--- src/main/java/jp/co/ambitious/sample/nba/mappers/PlayerMapper.java
+++ src/main/java/jp/co/ambitious/sample/nba/mappers/PlayerMapper.java
@@ -3,6 +3,7 @@ package jp.co.ambitious.sample.nba.mappers;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
 
 import jp.co.ambitious.sample.nba.beans.Player;
 import jp.co.ambitious.sample.nba.forms.SearchForm;
@@ -15,5 +16,20 @@ public interface PlayerMapper {
      * @return 選手情報リスト
      */
     public List<Player> selectAll(SearchForm searchForm);
+
+    /**
+     * 更新機能で引数を元にフィルタリングしたレコードを取得する
+     * @param playerNo
+     * @return
+     */
+    public Player selectByPK(int playerNo);
+
+    /**
+     * 本アプリの更新機能。xmlファイルに書かずMapperインターフェースに直接記載。
+     * @param playerNo
+     * @return
+     */
+    @Update("UPDATE m_player SET player_name = #{playerName}, team_id = #{team.teamId}, player_height = #{playerHeight}, player_age = #{playerAge}, player_position = #{playerPosition}, player_score = #{playerScore}, player_asist = #{playerAsist}, player_rebound = #{playerRebound}, version = 1 + version, up_dt = now() WHERE player_no = #{playerNo} AND version= #{version}")
+    public int updateByPK(Player player);
 
 }
--- src/main/java/jp/co/ambitious/sample/nba/services/NbaService.java
+++ src/main/java/jp/co/ambitious/sample/nba/services/NbaService.java
@@ -1,8 +1,11 @@
 package jp.co.ambitious.sample.nba.services;
 
 import java.util.List;
+import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 
 import jp.co.ambitious.sample.nba.beans.Player;
@@ -15,6 +18,9 @@ import jp.co.ambitious.sample.nba.mappers.TeamMapper;
 public class NbaService {
     
     @Autowired
+    MessageSource messageSource;
+
+    @Autowired
     PlayerMapper playerMapper;
 
     @Autowired
@@ -25,8 +31,28 @@ public class NbaService {
         return playerMapper.selectAll(searchForm);
     }
 
+    // playerの主キー(playNo)を元にレコードを取得
+    public Player selectByPK(int playerNo) {
+        return playerMapper.selectByPK(playerNo);
+    }
+
     // ドロップボックスにチーム名をセット
     public List<Team> getTeam() {
         return teamMapper.getTeam();
     }
+
+    // 保存機能
+    public void save(Player player) {
+        
+        int cnt = playerMapper.updateByPK(player);
+
+        if (cnt == 0) {
+            throw new OptimisticLockingFailureException(messageSource.getMessage("error.OptimisticLockingFailureException",null, Locale.JAPAN));
+        }
+        // なぜ注意文字が表示されないか分からないから元方ラグで内容確認しましよう
+        if (cnt > 1) {
+            throw new RuntimeException(messageSource.getMessage("error.RuntimeException", null, Locale.JAPAN));
+        }
+    }
+    
 }
